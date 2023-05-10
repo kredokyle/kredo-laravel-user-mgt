e04df6a58493cd086420cc0d6f7e6b735fc52f6b
kylenurville <kylenurvillejaham@gmail.com>
2023-05-10 19:13:49 +0800

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Storage;
+
+class UserController extends Controller
+{
+    private $user;
+    const LOCAL_STORAGE_FOLDER = 'public/avatars/';
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function home()
+    {
+        $all_users = $this->user->all();
+        return view('home')->with('all_users', $all_users);
+    }
+
+    public function create()
+    {
+        return view('create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|max:50',
+            'email' => 'required|email|max:50|unique:users'
+        ]);
+
+        $this->user->name = $request->name;
+        $this->user->email = $request->email;
+        $this->user->save();
+
+        return redirect()->route('home');
+    }
+
+    public function edit($id)
+    {
+        $user = $this->user->findOrFail($id);
+        return view('edit')->with('user', $user);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name'  => 'required|max:50',
+            'email' => 'required|email|max:50|unique:users,email,' . $id,
+            'avatar'=> 'mimes:jpg,png,jpeg,gif|max:1048'
+        ]);
+
+        $user           = $this->user->findOrFail($id);
+        $user->name     = $request->name;
+        $user->email    = $request->email;
+
+        if ($request->avatar){
+            if ($user->avatar){
+                $this->deleteAvatar($user->avatar);
+            }
+
+            $user->avatar = 'data:image/' . $request->avatar->extension() . ';base64,' . base64_encode(file_get_contents($request->avatar));
+        }
+
+        $user->save();
+
+        return redirect()->route('home');
+    }
+
+    private function deleteAvatar($image_name)
+    {
+        $avatar_path = self::LOCAL_STORAGE_FOLDER . $image_name;
+
+        if (Storage::disk('local')->exists($avatar_path)){
+            Storage::disk('local')->delete($avatar_path);
+        }
+    }
+
+    // private function saveAvatar($request)
+    // {
+    //     $avatar_name = time() . "." . $request->avatar->extension();
+
+    //     $request->avatar->storeAs(self::LOCAL_STORAGE_FOLDER, $avatar_name);
+
+    //     return $avatar_name;
+    // }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+    <h1>Welcome!</h1>
+    
+    <div class="text-end">
+        <a href="{{ route('create') }}">New User</a>
+    </div>
+    
+    <table class="table table-hover table-sm align-middle">
+        <thead>
+            <tr>
+                <th></th>
+                <th>NAME</th>
+                <th>EMAIL</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($all_users as $user)
+            <tr>
+                <td>
+                    @if ($user->avatar)
+                        <img src="{{ $user->avatar }}" alt="{{ $user->avatar }}" class="border p-1 d-block mx-auto">
+                    @else
+                        <div class="text-center">
+                            <i class="fa-solid fa-user fa-4x" class="text-dark"></i>
+                        </div>
+                    @endif
+                </td>
+                <td>{{ $user->name }}</td>
+                <td>{{ $user->email }}</td>
+                <td>
+                    <a href="{{ route('edit', $user->id) }}">Edit</a>
+                    &middot;
+                    <a href="#">Delete</a>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
